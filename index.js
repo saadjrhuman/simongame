3c6f1a83e0335b1c0a74647c5e8272026dfb020c
saadjrhuman <62991193+saadjrhuman@users.noreply.github.com>
2020-04-20 21:39:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var clrSqnc = ["red", "blue", "green", "yellow"];
+
 var ursChsnSqnc = [];
 var gmPttrn = [];
-var clrSqnc = ["red", "blue", "green", "yellow"];
 
 var strt = false;
 var lvl = 0;
@@ -19,9 +20,33 @@ $(".btn").click(function () {
   var usrChsnId = "#" + usrChsnClr;
   anmtPrs(usrChsnId);
   plSnd(usrChsnClr);
+
+  chckAns(ursChsnSqnc.length - 1);
 });
 
+function chckAns(crntLvl) {
+  if (gmPttrn[crntLvl] === ursChsnSqnc[crntLvl]) {
+    console.log("success");
+    if (gmPttrn.length === ursChsnSqnc.length) {
+      setTimeout(function () {
+        ranSqnc();
+      }, 1000);
+    }
+  } else {
+    console.log("fail");
+    plSnd("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over! Press any key to restart.");
+
+    strtOvr();
+  }
+}
+
 function ranSqnc() {
+  ursChsnSqnc = [];
   lvl++;
   $("#level-title").text("Level " + lvl);
   var ri = Math.floor(Math.random() * 4);
@@ -44,4 +69,10 @@ function anmtPrs(Id) {
   setTimeout(function () {
     $(Id).removeClass("pressed");
   }, 100);
+}
+
+function strtOvr() {
+  gmPttrn = [];
+  strt = false;
+  lvl = 0;
 }
